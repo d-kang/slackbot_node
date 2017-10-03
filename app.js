fe6f9e0080baeeb2ae42380e1f6649996aa1ea1a
d-kang <david.kang714@gmail.com>
2017-10-03 10:26:53 -0700

    Basic Infrastructure of State Machine and Handlers

--- app.js
+++ app.js
@@ -12,18 +12,52 @@ const rtm = new RtmClient(token, {
   autoMark: true,
 });
 
+let state = null;
+
+const setState = (newState) => {
+  state = newState;
+}
+
+const handlers = {};
+
+// basic router, can use botkit later on
+const router = (message) => {
+  if (!state) {
+    handlers.DEFAULT(message);
+  } else {
+    handlers[state](message);
+  }
+}
+
+handlers.DEFAULT = (message) => {
+  rtm.sendMessage("Welcome! What's your name", message.channel);
+  setState(states.GET_NAME);
+}
+
+/*
+  1. add a new handler called GET_NAME
+  2. have it collect and store the name of the user
+  3. have it set the state to phone number
+  4. collect and store the phone number
+  5. do the same for collecting an address
+  6. confirm with the user the input that you've stored
+  7. if yes, say thanks, and loop back to the beginning, otherwise loop back with error
+*/
+
 // Listens to all `message` events from the team
 rtm.on(RTM_EVENTS.MESSAGE, (message) => {
-  console.log('message', message)
+  // console.log('message', message)
   if (message.text.indexOf('time') > -1) {
     rtm.sendMessage('The time is ' + new Date().toLocaleTimeString(), message.channel)
   }
   if (message.channel === "G79CLHEA3") {
     console.log('Bot sees a message in general and chooses not to respond')
   } else {
-    rtm.sendMessage('Beep boop hello world!', message.channel)
-    rtm.sendMessage(`Hi there @${message.user}`, message.channel)
+    // rtm.sendMessage('Beep boop hello world!', message.channel)
+    // rtm.sendMessage(`Hi there @${message.user}`, message.channel)
+    router(message);
   }
 })
 
+// setState("DEFAULT");
 rtm.start();
