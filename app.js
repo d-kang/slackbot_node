93a9c71b4484bf099490cefb87f78900d90a9bc9
d-kang <david.kang714@gmail.com>
2017-10-04 09:58:57 -0700

    Day 2 Start Clean Repo And Exercise

--- app.js
+++ app.js
@@ -1,118 +1,100 @@
-const { RtmClient, MemoryDataStore, RTM_EVENTS } = require('@slack/client');
-const token = process.env.SLACK_TOKEN || '';
+const RtmClient = require('@slack/client').RtmClient;
+const MemoryDataStore = require('@slack/client').MemoryDataStore;
+const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+const token = process.env.SLACK_TOKEN || "";
+const witToken = process.env.WIT_ACCESS_TOKEN || "";
+const witAppID = process.env.WIT_APP_ID || "";
+const {Wit, log} = require("node-wit");
+
+const wit = new Wit({accessToken: witToken});
 
 const rtm = new RtmClient(token, {
-  logLevel: 'error',
-  // logLevel: 'debug',
-  // Initialise a data store for our client, this will load additional helper functions for the storing and retrieval of data
-  dataStore: new MemoryDataStore(),
-  // Boolean indicating whether Slack should automatically reconnect after an error response
-  autoReconnect: true,
-  // Boolean indicating whether each message should be marked as read or not after it is processed
-  autoMark: true,
+    logLevel: 'error',
+    // logLevel: 'debug',
+    // Initialise a data store for our client, this will load additional helper functions for the storing and retrieval of data
+    dataStore: new MemoryDataStore(),
+    // Boolean indicating whether Slack should automatically reconnect after an error response
+    autoReconnect: true,
+    // Boolean indicating whether each message should be marked as read or not after it is processed
+    autoMark: true
 });
 
-let state = {
-  name: '',
-  phoneNumber: '',
-  address: '',
+let data = {};
 
+let state = null;
+const setState = (newState) => {
+    state = newState;
 };
 
-let event = null;
-
-const setState = (oldState, newState) => {
-  state = Object.assign({}, oldState, newState);
-}
-const setEvent = (next) => {
-  event = next;
-}
-
 const handlers = {};
-
-// basic router, can use botkit later on
-const router = (message) => {
-  if (!event) {
-    handlers.DEFAULT(message);
-  } else {
-    handlers[event](message);
-  }
-}
-
 handlers.DEFAULT = (message) => {
-  rtm.sendMessage("Welcome! What's your name", message.channel);
-  setEvent('GET_NAME');
-}
-
-/*
-  1. add a new handler called GET_NAME
-  2. have it collect and store the name of the user
-  3. have it set the state to phone number
-  4. collect and store the phone number
-  5. do the same for collecting an address
-  6. confirm with the user the input that you've stored
-  7. if yes, say thanks, and loop back to the beginning, otherwise loop back with error
-*/
+    data = {};
+    rtm.sendMessage("Welcome! What's your name?", message.channel);
+    setState("GET_NAME");
+};
 
 handlers.GET_NAME = (message) => {
-  setState(state, {name: message.text});
+    data.name = message.text;
+    rtm.sendMessage(`${data.name} is a nice name. What is your phone number?`, message.channel);
+    setState("GET_PHONE_NUMBER");
+};
 
-  rtm.sendMessage(`Nice to meet you ${state.name}`, message.channel);
-  rtm.sendMessage(`What is your phone number?`, message.channel);
-  setEvent('GET_NUMBER')
-}
-
-handlers.GET_NUMBER = (message) => {
-  setState(state, {phoneNumber: message.text});
-
-  rtm.sendMessage(`Thanks for your phone number! ${state.phoneNumber}`, message.channel);
-  rtm.sendMessage(`What is your address?`, message.channel);
-  setEvent('GET_ADDRESS')
-}
+handlers.GET_PHONE_NUMBER = (message) => {
+    data.phoneNumber = message.text;
+    rtm.sendMessage(`Where do you life?`, message.channel);
+    setState("GET_ADDRESS");
+};
 
 handlers.GET_ADDRESS = (message) => {
-  setState(state, {address: message.text});
-
-  rtm.sendMessage(`Got it! ${state.address}`, message.channel);
-  rtm.sendMessage(`Is all this information correct? ${state.address}`, message.channel);
-  rtm.sendMessage(
-    `name: ${state.name}
-    phone-number: ${state.phoneNumber}
-    address: ${state.address}`, message.channel);
-  setEvent('CONFIRM')
-}
+    data.address = message.text;
+    rtm.sendMessage(`So you are ${data.name} from ${data.address} and i can call you on ${data.phoneNumber}. Is this correct?`, message.channel);
+    setState("CONFIRM");
+};
 
 handlers.CONFIRM = (message) => {
-  const isConfirmed = message.text.toLowerCase();
-  console.log('isConfirmed', isConfirmed);
-  if (isConfirmed.indexOf('yes') > -1) {
-    rtm.sendMessage('Great glad it was all correct!', message.channel);
-  } else {
-    rtm.sendMessage('oh noes! lets start over', message.channel);
-  }
-  setEvent(null);
-}
+    if(message.text.toLowerCase() === "yes") {
+        rtm.sendMessage(`Thanks ${data.name}.`, message.channel);
+        setState("DEFAULT");
+    } else {
+        rtm.sendMessage(`Ohh. Something went wrong :( Let's try this again.`, message.channel);
+        setState("DEFAULT");
+        handlers[state](message);
+    }
+};
+
+const router = (message) => {
+    if(!state) {
+        handlers.DEFAULT(message);
+    } else {
+        handlers[state](message);
+    }
+};
 
 /*
-  1. Validate the phone number
-  2. Either confirm and pass them to the next step
-  3. Or ask them for it again with an error message
+  Add new entity types to wit to accomplish the following task:
+  capture where, when and how much, asking them for what you don’t receive,
+  storing and confirming
 */
-
-
 
 // Listens to all `message` events from the team
 rtm.on(RTM_EVENTS.MESSAGE, (message) => {
-  if (message.channel === "G79CLHEA3") {
-    console.log('Bot sees a message in general and chooses not to respond')
-  } else {
-    if (message.text.indexOf('time') > -1) {
-      rtm.sendMessage('The time is ' + new Date().toLocaleTimeString(), message.channel)
-    }
-    // rtm.sendMessage('Beep boop hello world!', message.channel)
-    // rtm.sendMessage(`Hi there @${message.user}`, message.channel)
-    router(message);
-  }
-})
+    if(message.channel === 'G79CLHEA3') {
+        // console.log('Bot sees a message in General and ignores it.');
+    } else {
 
+      wit.message(message.text, {})
+        .then((data) => {
+          console.log('data', data);
+          if (data && data.entities && data.entities.location) {
+            console.log('wit thinks the location is: ', data.entities.location[0].value);
+
+          }
+        })
+        .catch(console.error)
+
+      // router(message);
+    }
+});
+
+setState("DEFAULT");
 rtm.start();
